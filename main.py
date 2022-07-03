2301061152e2aad10ea1ea453eb42f80cbe2a6dc
Mauricio Neut <m.neut.santos@gmail.com>
2022-07-02 22:28:14 -0400

    Creación Clase Food; nuevos métodos en Clase Snake; actualización main.py; actualización .gitignore

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from curses import keyname
 import pygame
+from snake import *
+from food import *
 
 pygame.init()
 bounds = (300, 300)
 window = pygame.display.set_mode(bounds)
 pygame.display.set_caption("Snake")
+block_size = 20
+snake = Snake(block_size, bounds)
+food = Food(block_size, bounds)
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        snake.steer(Direction.LEFT)
+    elif keys[pygame.K_RIGHT]:
+        snake.steer(Direction.RIGHT)
+    elif keys[pygame.K_UP]:
+        snake.steer(Direction.UP)
+    elif keys[pygame.K_DOWN]:
+        snake.steer(Direction.DOWN)
+    snake.move()    
+    window.fill((0, 0, 0))
+    snake.draw(pygame, window)
+    food.draw(pygame, window)
+    pygame.display.flip()
--- snake.py
+++ snake.py
@@ -10,7 +10,8 @@ class Direction(Enum):
 class Snake:
     length = None
     direction = None
-    body = Noneblock_size = None
+    body = None
+    block_size = None
     color = (0, 0, 255)
     bounds = None
     
@@ -21,5 +22,37 @@ class Snake:
 
     def respawn(self):
         self.length = 3
-        self.body = [(20,20), (20,40), (20,60)]
+        self.body = [(20,20), (20,40), (20,60)]  ##No degería referirlo a block_size (y sus múltiplos)?
         self.direction = Direction.DOWN
+
+    def draw(self, game, window):
+        for segment in self.body:
+            game.draw.rect(window, self.color, (segment[0], segment[1], self.block_size, self.block_size))
+
+    def move(self):
+        curr_head = self.body[-1]
+        if self.direction == Direction.DOWN:
+            next_head = (curr_head[0], curr_head[1] + self.block_size)
+            self.body.append(next_head)
+        elif self.direction == Direction.UP:
+            next_head = (curr_head[0], curr_head[1] - self.block_size)
+            self.body.append(next_head)
+        elif self.direction == Direction.RIGHT:
+            next_head = (curr_head[0] + self.block_size, curr_head[1])
+            self.body.append(next_head)
+        elif self.direction == Direction.LEFT:
+            next_head = (curr_head[0] - self.block_size, curr_head[1])
+            self.body.append(next_head)
+
+        if self.length < len(self.body):
+            self.body.pop(0)
+
+    def steer(self, direction):
+        if self.direction == Direction.DOWN and direction != Direction.UP:
+            self.direction = direction
+        elif self.direction == Direction.UP and direction != Direction.DOWN:
+            self.direction = direction
+        elif self.direction == Direction.LEFT and direction != Direction.RIGHT:
+            self.direction = direction
+        elif self.direction == Direction.RIGHT and direction != Direction.LEFT:
+            self.direction = direction            
